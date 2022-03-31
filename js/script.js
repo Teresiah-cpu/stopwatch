06736b407bb4ef70a3b27c323aaab50b90082a8c
Teresiah-cpu <terrywambo2018@gmail.com>
2022-03-31 21:54:28 +0300

    modify script file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
-alert="hey there"
+var seconds = 00;
+var tens = 00;
+var OutputSeconds = document.getElementById('second');
+var OutputTens = document.getElementById('tens');
+var buttonStart = document.getElementById('btn-start');
+var buttonPause = document.getElementById('btn-pause');
+var buttonReset = document.getElementById('btn-reset');
+
+var Interval;
+
+buttonStart.addEventListener('click', () =>{
+      clearInterval(Interval);
+      Interval = setInterval(startTime, 10);
+
+
+
+
+});
+
+buttonPause.addEventListener( 'click', () =>{
+    clearInterval(Interval);
+});
+
+buttonReset.addEventListener( 'click', () =>{
+    clearInterval(Interval);
+    tens ="00";
+    seconds ="00";
+    OutputSeconds.innerHTML = seconds;
+    OutputTens.innerHTML = tens;
+});
+
+function startTime(){
+    tens++;
+    if(tens<=9){
+        OutputTens.innerHTML = "0" + tens;
+    }
+    if(tens>9){
+        OutputTens.innerHTML = tens;
+    }
+    if(tens >99){
+        seconds++;
+        OutputSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        OutputTens.innerHTML = "0" + tens;
+    }
+    if(seconds > 9){
+        OutputSeconds.innerHTML = seconds;
+    }
+}
+
+
+ 
